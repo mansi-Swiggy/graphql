0169c3f8417f84eaeb4f1dcae582e20bf94962f8
aswigy <101206481+aswigy@users.noreply.github.com>
2023-05-16 09:09:10 +0000

    Run both HTTP and GraphQL

--- server.go
+++ server.go
@@ -18,40 +18,22 @@ const defaultGRPCPort = 8080
 const defaultHTTPPort = 8089
 
 func main() {
-	// defer func ()  {
-	// 	serverGraphQL(defaultGRPCPort)
-	// 	serveHTTP(defaultHTTPPort)
-	// }()
-
-	for {
-		serverGraphQL(defaultGRPCPort)
-		serveHTTP(defaultHTTPPort)
-	}
-
-	// go 
-	// go 
-
-}
-
-func serverGraphQL(port int) {
-
 	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
 
-	zap.L().Info("connect to GraphQL playground", logging.AutoField("port", port))
+	zap.L().Info("connect to GraphQL playground", logging.AutoField("port", defaultGRPCPort))
 
-	if err := http.ListenAndServe(":"+strconv.Itoa(port), nil); err != nil {
-		zap.L().Error("Server error", zap.Error(err))
-
-	}
-
-}
-
-func serveHTTP(port int) {
 	r := gin.Default()
 	r.GET(api.HealthCheckString, api.NewHealthCheckHandler().HealthCheck)
 	r.GET("/user", api.NewUserHandler().GetUser)
-	r.Run(":8089")
+
+	go func() {
+		r.Run(":8089")
+	}()
+	if err := http.ListenAndServe(":"+strconv.Itoa(defaultGRPCPort), nil); err != nil {
+		zap.L().Error("Server error", zap.Error(err))
+
+	}
 }
